e04750b55c023d853ec09e2e7fff0824386fae98
hysj <441717627@qq.com>
2022-10-31 10:16:02 +0800

    首次提交

---
+++ build/CMakeFiles/test.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test.dir/src/hello.obj"
+  "CMakeFiles/test.dir/src/hello.obj.d"
+  "E:/vscode/test/bin/libtest.dll.a"
+  "E:/vscode/test/bin/test.exe"
+  "E:/vscode/test/bin/test.exe.manifest"
+  "E:/vscode/test/bin/test.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
